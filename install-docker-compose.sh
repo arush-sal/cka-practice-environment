377ac17f61ad539cfc5a955cd4c9b82d87547b16
Xuefeng <yxfdental@126.com>
2020-06-12 00:02:38 +0800

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,6 +1,6 @@
-wget https://www.cnrancher.com/download/compose/v1.23.2-docker-compose-Linux-x86_64
+wget https://docs.rancher.cn/download/compose/v1.24.1-docker-compose-Linux-x86_64
 
-mv v1.23.2-docker-compose-Linux-x86_64 docker-compose
+mv v1.24.1-docker-compose-Linux-x86_64 docker-compose
 chmod +x docker-compose
 
 mv docker-compose /usr/local/bin/
